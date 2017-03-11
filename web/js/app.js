622164dd10d12d023324b2062c0f171e27334601
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2017-03-12 00:05:00 +0800

    Update

--- web/js/app.js
+++ web/js/app.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import reducer from './store'
-import { Provider } from 'react-redux'
-import Chrome from './components/chrome'
-import Router from './router'
-import { createStore, applyMiddleware } from 'redux'
 import createLogger from 'redux-logger'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import reducer from './store'
+import Chrome from './components/chrome'
+import Router from './services/router'
 import Ticker from './services/ticker'
 
 const store = createStore(reducer, {}, applyMiddleware(
