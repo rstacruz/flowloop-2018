5d8883626779130277042bc2c8bddf3f84df0b4c
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2017-03-13 22:15:29 +0800

    Add blank state

--- web/js/components/timeline_summary.jsx
+++ web/js/components/timeline_summary.jsx
@@ -6,19 +6,41 @@ import { recents } from '../log'
 
 class TimelineSummary extends React.Component {
   render () {
-    console.log('recents:', this.props.items)
+    const items = values(this.props.items)
+    const isEmpty = items.length === 0
+
     return <div className="timeline-summary fixed">
-      {values(this.props.items).map(item => {
-        return <span className={c('timeline-small-item', {
-          '-break': item.timerType === 'break',
-          '-work': item.timerType === 'work',
-        })}>
-          {item.label}
-        </span>
-      })}
+      {isEmpty
+        ? <Empty />
+        : items.map(item => <Item item={item} />)}
     </div>
   }
 }
+
+/*
+ * No items
+ */
+
+function Empty () {
+  return <span className='triangle' />
+}
+
+/*
+ * A single item
+ */
+
+function Item ({ item }) {
+  return <span className={c('timeline-small-item', {
+    '-break': item.timerType === 'break',
+    '-work': item.timerType === 'work',
+  })}>
+    {item.label}
+  </span>
+}
+
+/*
+ * Redux
+ */
 
 TimelineSummary = connect(
   state => ({
