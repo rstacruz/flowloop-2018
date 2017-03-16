fc1c21940385cdbfe2212b221dfb106b9fa6a6b2
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2017-03-16 23:52:28 +0800

    Update

--- web/js/components/label_selector.jsx
+++ web/js/components/label_selector.jsx
@@ -10,6 +10,7 @@ import React from 'react'
 
 class LabelSelector extends React.Component {
   /*:: props: Props */
+
   render () {
     return <strong className='label-selector'>
       <span className='label'>{this.props.label}</span>
