bb0f39b15b289c82425425ba139d97e3c9395fe3
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2017-03-20 23:51:00 +0800

    Tray icon

--- electron/index.js
+++ electron/index.js
@@ -1,54 +1,22 @@
-const electron = require('electron')
+const {app, Menu, Tray} = require('electron')
 const Menubar = require('menubar')
-
-const app = electron.app
 
 const ROOT = require('path').resolve(__dirname, '../')
 
 // adds debug features like hotkeys for triggering dev tools and reload
 // require('electron-debug')()
 
-// prevent window being garbage collected
-let mainWindow
+app.on('ready', () => {
+  // Tray icon
+  const tray = new Tray(`${ROOT}/node_modules/menubar/example/IconTemplate.png`)
+  tray.setTitle('0:37')
 
-// Menubar app
-const mb = Menubar({
-  index: `file://${ROOT}/public/index.html`,
-  preloadWindow: true,
-  width: 320,
-  height: 480
-})
-
-// function onClosed () {
-//   // dereference the window
-//   // for multiple windows store them in an array
-//   mainWindow = null
-// }
-
-function createMainWindow () {
-  const win = new electron.BrowserWindow({
+  // Menubar app
+  const mb = Menubar({
+    index: `file://${ROOT}/public/index.html`,
+    preloadWindow: true,
+    tray,
     width: 320,
     height: 480
   })
-
-  win.loadURL(`file://${ROOT}/public/index.html`)
-  win.on('closed', onClosed)
-
-  return win
-}
-
-// app.on('window-all-closed', () => {
-//   if (process.platform !== 'darwin') {
-//     app.quit()
-//   }
-// })
-
-// app.on('activate', () => {
-//   if (!mainWindow) {
-//     mainWindow = createMainWindow()
-//   }
-// })
-
-// app.on('ready', () => {
-//   mainWindow = createMainWindow()
-// })
+})
