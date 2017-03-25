22acfb44d1c585c0bac307cb6bd31c0c291eecec
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2017-03-25 18:02:38 +0800

    Display hours

--- web/js/helpers/timer_display.js
+++ web/js/helpers/timer_display.js
@@ -12,7 +12,7 @@ import leftPad from 'left-pad'
 export default function timerDisplay (
   ms /*: number */,
   isRemaining /*: ?boolean */) {
-  let mins, secs
+  let mins, secs, hours
   let positive = ms >= 0
 
   ms = Math.abs(ms)
@@ -21,8 +21,18 @@ export default function timerDisplay (
   mins = (secs / 60) | 0
   secs -= mins * 60
 
-  let secsPadded = leftPad(secs.toString(), 2, '0')
+  hours = (mins / 60) | 0
+  mins -= hours * 60
+
   let sign = positive ? '' : '-'
 
-  return `${sign}${mins}:${secsPadded}`
+  if (hours > 0) {
+    return `${sign}${hours}:${pad(mins)}:${pad(secs)}`
+  } else {
+    return `${sign}${mins}:${pad(secs)}`
+  }
+}
+
+function pad (num /*: number */) {
+  return leftPad(num.toString(), 2, '0')
 }
