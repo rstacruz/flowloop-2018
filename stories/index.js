30409c6dc0dca8ef7f974450b78a34b81deb4cd7
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2017-03-25 08:54:04 +0800

    Story book

---
+++ stories/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { storiesOf, action, linkTo } from '@kadira/storybook';
+import Button from './Button';
+import Welcome from './Welcome';
+
+storiesOf('Welcome', module)
+  .add('to Storybook', () => (
+    <Welcome showApp={linkTo('Button')}/>
+  ));
+
+storiesOf('Button', module)
+  .add('with text', () => (
+    <Button onClick={action('clicked')}>Hello Button</Button>
+  ))
